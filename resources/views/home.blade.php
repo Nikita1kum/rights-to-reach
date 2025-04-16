5c368fbe7d385ba5c4629d1aec6dcb60037cf672
Nikita1kum <124607024+Nikita1kum@users.noreply.github.com>
2025-04-17 01:16:35 +0530

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,7 +24,6 @@
 
         <a href="{{ route('login') }}"
            class="inline-block bg-gray-700 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded mr-2">
-           class="inline-block bg-gray-700 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded mr-2">
             Login
         </a>
 
